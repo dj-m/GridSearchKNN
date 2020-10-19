30dd266e7fd83a77ab412a86badb53c5c1c8d7bd
Domingo José Moronta <domingomoronta@gmail.com>
2020-10-19 13:12:12 -0700

    add data science cookiecutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Performing hyperparameter tuning using Grid Search with a K-Nearest Neighbor (KNN) machine learning model.',
+    author='Domingo Moronta',
+    license='MIT',
+)
